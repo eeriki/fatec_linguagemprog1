6c3bf43d1b36ca557d295081e915ea1be0aff56c
eEriki <62784411+eeriki@users.noreply.github.com>
2026-03-26 20:25:51 -0300

    Add files via upload

---
+++ Aula 07/C++/Ex3_2_2.cpp
@@ -0,0 +1,17 @@
+#include <stdio>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+void exibirMenu() {
+    cout << "------------------------------" << endl;
+    cout << "        MENU DE ESTOQUE       " << endl;
+    cout << "------------------------------" << endl;
+    cout << "1. Adicionar novo produto" << endl;
+    cout << "2. Listar produtos em estoque" << endl;
+    cout << "3. Sair" << endl;
+    cout << "------------------------------" << endl;
+}
